2b4252b0345dd33a832dfeeafd2f524ff880d5f0
johnson1123 <onifadejohnson2017@gmail.com>
2025-08-14 01:02:14 -0400

    Adjustment of services slider

--- src/components/medium/slider/HomeServicesSlider.tsx
+++ src/components/medium/slider/HomeServicesSlider.tsx
@@ -14,7 +14,7 @@ export default function HomeServicesSwiper() {
     setOpen(true);
   }, []);
   return (
-    <div className="w-[100%] h-[500px] 3k:h-[800px] overflow-hidden">
+    <div className="w-[100%] h-max md:h-[400px] 3k:h-[800px] overflow-hidden">
       <Swiper
         autoplay={true}
         speed={500}
